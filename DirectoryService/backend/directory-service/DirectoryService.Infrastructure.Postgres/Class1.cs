39fc4ee9d21730b7b66e50365dbaea32a581f119
uluana_roshina <uluanasev15@gmail.com>
2026-05-20 19:07:25 +0300

    connecting to DB

---
+++ DirectoryService/backend/directory-service/DirectoryService.Infrastructure.Postgres/Class1.cs
@@ -0,0 +1,100 @@
+﻿using Directory.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace DirectoryService.Infrastructure.Postgres;
+
+public class DirectoryServiceDbContext: DbContext
+{
+    public readonly string _connectionString;
+
+    public DirectoryServiceDbContext(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql();
+    }
+
+    public DbSet<Department> Departments => Set<Department>();
+}
+
+public class AppDbContext
+{
+    private readonly string _connectionString;
+
+    public AppDbContext(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public Task GetDepartments()
+    {
+        
+    }
+}
+
+
+
+
+
+public class DirectoryRepository
+{
+    private readonly DirectoryServiceDbContext _dbContext;
+    
+    public DirectoryRepository(DirectoryServiceDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    
+    public async Task AddDepartment(Department department)
+    {
+        // var connectionString = "Server=127.0.0.1;Port=5432;Database=myDataBase;User Id=myUsername;Password=myPassword;PostgreSQL"
+        //
+        // await using var dataSource = NpgsqlDataSource.Create(connectionString);
+        //
+        // var sql = "INSERT INTO departments (department_id, name) VALUES (@department_id, @name)";
+        //
+        // var command = dataSource.CreateCommand(sql);
+        // command.Parameters.Add(new NpgsqlParameter("id", department.Id));
+        // command.Parameters.Add(new NpgsqlParameter("name", department.Name));
+        //
+        // await command.ExecuteNonQueryAsync();
+        
+        
+        await _dbContext.AddAsync(department);
+    }
+    
+    public async Task<List<DepartmentDto>> GetDepartment()
+    {
+        // var connectionString = "Server=127.0.0.1;Port=5432;Database=myDataBase;User Id=myUsername;Password=myPassword;PostgreSQL"
+        //
+        // await using var dataSource = NpgsqlDataSource.Create(connectionString);
+        //
+        // var command = dataSource.CreateCommand("SELECT id, name FROM departments");
+        //
+        // await using var reader = await command.ExecuteReaderAsync();
+        //
+        // var departments = new List<DepartmentDto>();
+        //
+        // while (await reader.ReadAsync())
+        // {
+        //     var departmentDto = new DepartmentDto(reader.GetGuid(0), reader.GetString(1));
+        //     
+        //     departments.Add(departmentDto);
+        // }
+        //
+        // return departments;
+//var departments = await _dbContext.Set<Department>().Select(d =>new DepartmentDto()).ToListAsync()
+var departments = await _dbContext.Departments.Select(d =>new DepartmentDto(d.Id, d.Name)).ToListAsync()
+return departments;
+
+    }
+}
+
+public record DepartmentDto(Guid Id, string Name);
--- DirectoryService/backend/directory-service/src/DirectoryService.Domain/ValueObjects/DepartmentName.cs
+++ DirectoryService/backend/directory-service/src/DirectoryService.Domain/ValueObjects/DepartmentName.cs
@@ -1,32 +1,19 @@
 // ValueObjects/DepartmentName.cs
 namespace Directory.Domain.ValueObjects;
-
 public sealed record DepartmentName
 {
     public string Value { get; }
-
     private DepartmentName(string value) => Value = value;
-
     public static DepartmentName Create(string value)
     {
         if (string.IsNullOrWhiteSpace(value))
             throw new ArgumentException("Название не может быть пустым", nameof(value));
-
         value = value.Trim();
-
         if (value.Length < 2)
             throw new ArgumentException("Название слишком короткое (минимум 2 символа)", nameof(value));
-
         if (value.Length > 100)
             throw new ArgumentException("Название слишком длинное (максимум 100 символов)", nameof(value));
-
         return new DepartmentName(value);
     }
-    
-    public override bool Equals(object? obj) =>
-        obj is DepartmentName other && Value == other.Value;
-
-    public override int GetHashCode() => Value.GetHashCode();
-
     public override string ToString() => Value;
 }
